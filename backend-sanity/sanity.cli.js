b0bdb1c49511cb8ef67e11bb5f1560813cbad9f0
Dmitry A <adcntt@gmail.com>
2023-01-11 04:19:08 +0300

    sanity

---
+++ backend-sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kj9226z2',
+    dataset: 'production'
+  }
+})
---
+++ backend-sanity/schemas/index.js
@@ -0,0 +1,21 @@
+// schema types from any plugins that might expose them
+
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import works from './works'
+import workExperience from './workExperience'
+
+export const schemaTypes = [
+  abouts,
+  brands,
+  contact,
+  testimonials,
+  skills,
+  experiences,
+  works,
+  workExperience,
+]
