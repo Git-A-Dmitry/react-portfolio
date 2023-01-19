761d897e906d52e296141b7af3658e9f662a8520
Dmitry A <adcntt@gmail.com>
2023-01-19 17:29:47 +0300

    skills section

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,7 +1,5 @@
-import { About, Footer, Skills, Testimonial } from './container';
-import Header from './container/Header/Header';
+import { Header, About, Footer, Work, Skills, Testimonial } from './container';
 import Navbar from './components/Navbar/Navbar';
-import Work from './container/Work/Work';
 
 import './App.scss';
 
--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
+
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 const Skills = () => {
   const [experience, setExperience] = useState([]);
@@ -30,10 +32,12 @@ const Skills = () => {
         <motion.div className='app__skills-list'>
           {skills.map((skill) => (
             <motion.div //
+              key={skill.name}
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: [0.5] }}
               className='app__skills-item app__flex'
-              key={skill.name}
+              // id={skill.id}
+              // key={skill.name}
             >
               <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
                 <img src={urlFor(skill.icon)} alt={skill.name} />
@@ -42,9 +46,50 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+
+        <motion.div className='app__skills-exp'>
+          {/* {console.log('here', experience.works)} */}
+          {experience.map((experience) => (
+            <motion.div //
+              className='app__skills-exp-item'
+              key={experience.year}
+            >
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{experience.year}</p>
+              </div>
+
+              <motion.div className='app__skills-exp-works'>
+                {experience.works.map((work, index) => (
+                  <div key={work.name}>
+                    <motion.div //
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: [0.5] }}
+                      className='app__skills-exp-work'
+                      data-tip
+                      data-for={work.name}
+                      // key={work.name}
+                    >
+                      <h4 id={work.name} className='bold-text'>
+                        {work.name}
+                      </h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+
+                    <Tooltip //
+                      anchorId={work.name}
+                      place='top'
+                      className='skills-tooltip'
+                      content={work.desc}
+                    />
+                  </div>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   );
 };
 
-export { Skills };
+export default AppWrap(Skills, 'skills');
